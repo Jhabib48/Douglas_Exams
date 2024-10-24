40762e94f42736d6f596cf3bf34bd5e6fde43ef0
Jhabib48 <j_habib@outlook.com>
2024-10-24 13:38:26 -0700

    Added: added student controller and added data to h2 database for student login

--- src/main/java/com/csis3275/DouglasExamProjectApplication.java
+++ src/main/java/com/csis3275/DouglasExamProjectApplication.java
@@ -1,13 +1,25 @@
 package com.csis3275;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.csis3275.model_hja_73.DouglasStudent;
+import com.csis3275.model_hja_73.StudentDAO;
 
 @SpringBootApplication
 public class DouglasExamProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DouglasExamProjectApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner douglasStudentData( StudentDAO doa) {
+		return (args) -> {
+			doa.addDouglasStudent( new DouglasStudent("John", "Doe", "CSIS", "test@hotmail.ca")); 
+		}; 
 	}
 
 }
--- src/main/java/com/csis3275/controller_hja_73/DouglasStudentController.java
+++ src/main/java/com/csis3275/controller_hja_73/DouglasStudentController.java
@@ -1,5 +1,52 @@
 package com.csis3275.controller_hja_73;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.csis3275.model_hja_73.DouglasStudent;
+import com.csis3275.model_hja_73.StudentDAO;
+
+// This is for student data stored for login 
+
+@RestController
+@RequestMapping("/students")
 public class DouglasStudentController {
 
+	@Autowired
+	StudentDAO studentDAO;
+	
+	// List student
+	@GetMapping("/list")
+	public List<DouglasStudent> studentList() {
+		return studentDAO.readDouglasStudent();
+	}
+	
+	//Check if the user being created exist - email 
+	//Add student
+	@GetMapping("/add")
+	public DouglasStudent addStudent( DouglasStudent student) {
+		return studentDAO.addDouglasStudent(student); 
+	}
+	
+	//Delete
+	@GetMapping("/delete")
+	public void deleteStudent( DouglasStudent student) {
+		 studentDAO.deleteDouglasStudent(student.getId());
+	}
+	
+	//Find
+	@GetMapping("/find")
+	public DouglasStudent findStudent(DouglasStudent student) {
+		return studentDAO.findDouglasStudent(student.getId());
+	}
+	
+	//Update
+	@GetMapping("/update")
+	public DouglasStudent updateDouglasStudent(DouglasStudent student) {
+		return studentDAO.updateDouglasStudent(student); 
+	}
 }
